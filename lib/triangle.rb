0b24fa4fd709aa256e131a7d3dfec629885b251f
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-01-25 05:38:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,21 +21,21 @@ class Triangle
 
    def kind
      if valid?
-       if @triangle_sides.uniq.length == 1
-         return :equilateral
-       elsif @triangle_sides.uniq.length == 2
-         return :isosceles
-       else
-         return :scalene
-       end
+        if @triangle_sides.uniq.length == 1
+          return :equilateral
+        elsif @triangle_sides.uniq.length == 2
+          return :isosceles
+        else
+          return :scalene
+        end
      else
-       raise TriangleError
-     end
-   end
- end
+        raise TriangleError
+      end
+    end
+  end
 
- class TriangleError < StandardError
+  class TriangleError < StandardError
 
- end
+  end
  
 end 
